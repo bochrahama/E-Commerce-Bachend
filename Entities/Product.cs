de1f4a9e9d169c4eb867ce2f4cb43fb2d3136f93
Bochra <Bochrahiba@outlook.com>
2026-08-15 08:24:41 +0100

    build Product Class to represent a product in the e-commerce system with properties

---
+++ Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EcommerceBackend.Entities
+{
+    public class Product
+    {
+        
+        public int ProductId { get; private set; }
+       
+        public string ProductName { get; set; } = string.Empty;
+        public string? ProductDescription { get; set; } 
+        public decimal ProductPrice { get; set; } 
+        public decimal? ProductQuantity { get; set; }
+        public  DateTime ProductCreatedAt { get; set; } = DateTime.UtcNow;
+        public bool ProductIsActive { get; set; } = true;
+        public Category ProductCategory { get; set; } 
+        public int ProductCategoryId { get; set; }
+        public ICollection <ProductImage> ProductImages { get; set; } = new List<ProductImage>();
+        public Product() { }
+
+
+    }
+}
