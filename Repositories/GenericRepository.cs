8dcced33b9e22727e58a3acab73aaa015714bcec
Bochra <Bochrahiba@outlook.com>
2026-08-16 12:21:48 +0100

    build the Repositories

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EcommerceBackend.Data;
+
+namespace EcommerceBackend.Repositories
+{
+    public class GenericRepository<T, TKey> : IGenericRepository<T, TKey> where T : class
+    {
+        //_appDbContext is 
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
+        }
+        public async Task<T?> GetByIdAsync(TKey id) => await _dbSet.FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+        public async Task AddSync(T entity) => await _dbSet.AddAsync(entity);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Remove(T entity) => _dbSet.Remove(entity);
+        public async Task<bool> SaveChangesAsync() => await _appDbContext.SaveChangesAsync()>0;
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace EcommerceBackend.Repositories
+{
+    // Generic repository interface for CRUD operations
+    //the porpose of this interface is to provide a common set of methods for performing CRUD operations on entities of type T,
+    //where T is a class. The interface defines methods for retrieving entities by their unique identifier (GetByIdAsync),
+    //retrieving all entities (GetAllAsync), adding new entities (AddSync), updating existing entities (Update), removing entities (Remove),
+    //and saving changes to the underlying data store (SaveChangesAsync).
+    //By using this generic repository interface, developers can create concrete implementations for
+    //specific entity types, promoting code reusability and maintainability in the application.
+    public interface IGenericRepository<T , TKey>  where T : class
+    {
+        // Retrieves an entity of type T by its unique identifier asynchronously.
+        Task<T?> GetByIdAsync(TKey id);
+        // Retrieves all entities of type T from the data store asynchronously.
+        Task<IEnumerable<T>> GetAllAsync();
+        // Adds a new entity of type T to the data store asynchronously.
+        Task AddSync(T entity);
+        // Updates an existing entity of type T in the data store asynchronously.
+        void Update(T entity);
+        // Removes an entity of type T from the data store asynchronously.
+        void Remove (T entity);
+        // Saves changes made to the data store asynchronously and returns a boolean indicating success or failure 
+        Task<bool> SaveChangesAsync ();
+    }
+}
