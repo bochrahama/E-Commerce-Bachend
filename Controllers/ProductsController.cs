71c190c78ae32c669bc3374ada5693c7cba5a0c1
Bochra <Bochrahiba@outlook.com>
2026-09-11 12:25:22 +0100

    build the controller for the product entity with all CRUD operations and filtering by category and price range

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EcommerceBackend.Services;
+using EcommerceBackend.Entities;
+namespace EcommerceBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            [FromQuery] string? search ,
+            [FromQuery] int? categoryId,
+            [FromQuery] decimal? minPrice ,
+            [FromQuery] decimal? maxPrice )
+        {
+            var products = await _productService.GetAllAsync(search, categoryId, minPrice, maxPrice);
+            return Ok(products);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            var createdProduct = await _productService.CreatAsync(product);
+            return CreatedAtAction(nameof(GetById), new { id = createdProduct.ProductId }, createdProduct);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+            var updatedProduct = await _productService.UpdateAsync(product);
+            if (updatedProduct == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedProduct);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _productService.DeleteAsync(id);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
