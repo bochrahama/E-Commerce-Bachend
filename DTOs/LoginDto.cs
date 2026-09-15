eab4491a1255737b697a76754e9d630055690d1b
Bochra <Bochrahiba@outlook.com>
2026-09-15 14:41:40 +0100

    build the LoginDto class to represent the data needed for user login

---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿//build the LoginDto class to represent the data needed for user login.
+namespace EcommerceBackend.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
