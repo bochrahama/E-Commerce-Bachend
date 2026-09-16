0db22714b1b5272730c228d6e15e856ad4f3a4ab
Bochra <Bochrahiba@outlook.com>
2026-09-16 10:51:25 +0100

    build it to handle user authentication and authorization, including registration and login functionality

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EcommerceBackend.Services;
+using EcommerceBackend.Entities;
+using Microsoft.AspNetCore.Identity;
+using EcommerceBackend.DTOs;
+namespace EcommerceBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ITokenService _tokenService;
+
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {   //user for registration the user manager to create a new user and add it to the database
+            var user = new AppUser
+            {
+                UserName = registerDto.Email,
+                Email = registerDto.Email,
+                FullName = registerDto.FullName
+            };
+            //result of the user creation, if it fails return bad request with the errors
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            await _userManager.AddToRoleAsync(user, "User");
+            return Ok(new { message = "User registered successfully" });
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login( LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+            {
+                return BadRequest("Invalid email or password");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded)
+            {
+                return BadRequest("Invalid email or password");
+            }
+            // Generate a JWT token for the authenticated user and return it in the response
+            return Ok(new { token = await _tokenService.CreateTokenAsync(user) });
+        }
+    }
+}
