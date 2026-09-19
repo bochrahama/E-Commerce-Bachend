8db1c50bd7018c14c2a7f4d6d6bbaf22282d52c7
Bochra <Bochrahiba@outlook.com>
2026-09-19 14:56:58 +0100

    Update Jws code in program

--- Program.cs
+++ Program.cs
@@ -1,26 +1,83 @@
 
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 using EcommerceBackend.Data;
 using EcommerceBackend.Entities;
 using EcommerceBackend.Repositories;
 using EcommerceBackend.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using Swashbuckle.AspNetCore.SwaggerUI;     
+
+    
 
 var builder = WebApplication.CreateBuilder(args);
-
+// Add services to the container. 
 builder.Services.AddControllers();
 IServiceCollection serviceCollection = builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<AppUser,IdentityRole>(options =>
+{
+    // Configure password requirements for Identity
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var Key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = jwtSettings["Issuer"],
+         ValidAudience = jwtSettings["Audience"],
+         IssuerSigningKey = new SymmetricSecurityKey(Key)
+     });
+builder.Services.AddAuthorization();
+
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Enter Bearer {your_token}",
+        Type = SecuritySchemeType.Http,
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+});
 builder.Services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>)) ;
 // Register the ProductServices and CategoryService with the DI container
 builder.Services.AddScoped<IProductService, ProductServices>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -30,11 +87,23 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+// Add authentication and authorization middleware
+// authentication is about confirming who you are, while authorization is about determining what you can do.
+app.UseAuthentication();
 app.UseAuthorization();
 
 
 app.MapControllers();
+var roleManager = app.Services.CreateScope().ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+// Create roles if they don't exist
+// the porpes is to ensure that the necessary roles are available in the system for user management and authorization purposes.
+foreach (var roleName in new[] { "Admin", "User" })
+{
+    if (!await roleManager.RoleExistsAsync(roleName))
+    {
+        await roleManager.CreateAsync(new IdentityRole(roleName));
+    }
+}
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
