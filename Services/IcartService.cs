1ebea3e517f304dea301fb7bb3949cbffd423d63
Bochra <Bochrahiba@outlook.com>
2026-09-19 14:56:33 +0100

    Update IcartService (type of method)

---
+++ Services/IcartService.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceBackend.DTOs;
+namespace EcommerceBackend.Services
+{
+    //it's for the cart service to manage the cart items and the cart itself
+    public interface ICartService
+    {
+        
+        Task<CartDto> GetCartAsync(string userId);
+        Task<CartDto> AddItemToCartAsync(string userId, CartItemDto cartItemDto);
+        Task<bool> UpdateQuantityAsync(string userId, int CartItemId , int Quantity);
+        Task<bool> RemoveItemFromCartAsync(string userId, int productId);
+        Task ClearCartAsync(string userId);
+
+    }
+}
