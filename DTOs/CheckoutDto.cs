16c3be925eac4b26d9c7b2af276d5d33ea309815
Bochra <Bochrahiba@outlook.com>
2026-09-20 10:19:37 +0100

    build the DTO for the checkout (the purpose is to transfer data between the client and the server)

---
+++ DTOs/CheckoutDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+//the pojo class for the checkout process, containing shipping address and name on card
+namespace EcommerceBackend.DTOs
+{
+    public class CheckoutDto
+    {
+        public string ShippingAddress { get; set; } = string.Empty;
+        public string NameOnCard { get; set; } = string.Empty;
+    }
+}
