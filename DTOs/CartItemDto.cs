9526ada05dda517c37d9492c3fbbad4744848695
Bochra <Bochrahiba@outlook.com>
2026-09-25 11:58:50 +0100

    update cartItemId type

--- DTOs/CartItemDto.cs
+++ DTOs/CartItemDto.cs
@@ -4,7 +4,7 @@
     public class CartItemDto
     {
         public int ProductId { get; set; }
-        public int CartItemId { get; set; }
+        public Guid CartItemId { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Quantity { get; set; }
